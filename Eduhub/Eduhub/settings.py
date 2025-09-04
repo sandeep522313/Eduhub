2a86a52c3eb7fa58bfae6e36e7a6f2094a5a3ec6
sandeep521313 <sandeep522313@gmail.com>
2025-09-04 16:13:26 +0530

    latest updated code

--- Eduhub/Eduhub/settings.py
+++ Eduhub/Eduhub/settings.py
@@ -24,9 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-u^=030i=o(ltpjh*&)i1s$_n9gyt!@_%_v0k*1iq!f5*q-qkka'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com']
+
+# ALLOWED_HOSTS = []
 
 
 # Application definition
